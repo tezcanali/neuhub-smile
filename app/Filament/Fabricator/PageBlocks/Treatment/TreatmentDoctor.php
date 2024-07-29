af2bbbc7494d6bd542e04f99b081e6001a4991ae
Ali Tezcan <alitezcan147@icloud.com>
2024-07-29 15:12:35 +0300

    V1.0.0 Section name update

--- app/Filament/Fabricator/PageBlocks/Treatment/TreatmentDoctor.php
+++ app/Filament/Fabricator/PageBlocks/Treatment/TreatmentDoctor.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Fabricator\PageBlocks\Treatment;
 
+use App\Models\Doctor;
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Select;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class TreatmentDoctor extends PageBlock
@@ -11,7 +13,11 @@ class TreatmentDoctor extends PageBlock
     {
         return Block::make('treatment.treatment-doctor')
             ->schema([
-                //
+                Select::make('doctor_id')
+                    ->label('Doctor')
+                    ->options(function () {
+                        return Doctor::all()->pluck('name', 'id')->toArray();
+                    })
             ])->visible(fn ($get) => $get('../layout') == 'treatment-detail')->label('19 Doctor');
     }
 
