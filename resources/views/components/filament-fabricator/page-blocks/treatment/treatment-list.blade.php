0ba688bb025ec4ebca75574b84d00949c3e3c6a2
Ali Tezcan <alitezcan147@icloud.com>
2024-08-31 11:27:02 +0300

    fix blog categories

--- resources/views/components/filament-fabricator/page-blocks/treatment/treatment-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/treatment/treatment-list.blade.php
@@ -1,18 +1,20 @@
 @aware(['page'])
 <div class="row g-4">
-    <div class="col-lg-4">
-        @if($url)
-            <a href="{{ $url }}" class="d-block w-100 bg-white rounded-3 p-3">
-                <div class="image mb-3">
-                    <img src="{{ \Illuminate\Support\Facades\Storage::url($image) }}"
-                         class="w-100 h-300px object-fit-cover rounded-1"/>
-                </div>
-                <div class="fs-5 fw-normal pb-2">{{ $title }}</div>
-                <span class="d-flex align-items-center fs-13 gap-2 pb-2">
+    @foreach($treatments as $item)
+        <div class="col-lg-4">
+            @if($item->url)
+                <a href="{{ $item->url }}" class="d-block w-100 bg-white rounded-3 p-3">
+                    <div class="image mb-3">
+                        <img src="{{ \Illuminate\Support\Facades\Storage::url($item->image }}"
+                             class="w-100 h-300px object-fit-cover rounded-1"/>
+                    </div>
+                    <div class="fs-5 fw-normal pb-2">{{ $item->title }}</div>
+                    <span class="d-flex align-items-center fs-13 gap-2 pb-2">
                                 <i class="fa fa-arrow-right"></i>
                                 View More *
                             </span>
-            </a>
-        @endif
-    </div>
+                </a>
+            @endif
+        </div>
+    @endforeach
 </div>
