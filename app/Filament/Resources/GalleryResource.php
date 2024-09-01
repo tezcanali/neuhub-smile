4624b8a4b7a77ef308fb0aadd4fab306c5a9f5d7
Ali Tezcan <alitezcan147@icloud.com>
2024-09-01 10:47:21 +0300

    update footer

--- app/Filament/Resources/GalleryResource.php
+++ app/Filament/Resources/GalleryResource.php
@@ -134,6 +134,7 @@ class GalleryResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
