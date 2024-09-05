c58b4ba7315889dcd2235f86af5d3f8658da1fd1
Ali Tezcan <alitezcan147@icloud.com>
2024-09-05 20:38:24 +0300

    fix front pages

--- resources/views/components/filament-fabricator/page-blocks/treatment/trust-pilot.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/treatment/trust-pilot.blade.php
@@ -5,17 +5,12 @@
         <div class="comment-content row g-lg-2 g-3 text-md-start text-center">
             <div class="col-lg-3">
                 <img src="{{asset('front/img/tp.png')}}" class="h-100px" />
-                <div class="pt-lg-5 pt-md-4 pt-3">Based on <a href="#" class="fw-normal text-decoration-underline">8,560 reviews</a></div>
             </div>
             <div class="col-lg-9">
                         <div class="trustpilot-widget" data-locale="en-GB" data-template-id="54ad5defc6454f065c28af8b" data-businessunit-id="62f21650759a69faaab38715" data-style-height="240px" data-style-width="100%" data-theme="light" data-stars="5" data-review-languages="en" data-font-family="Oxygen">
                             <a href="https://uk.trustpilot.com/review/smilecenterturkey.co" target="_blank" rel="noopener">Trustpilot</a>
                         </div>
                     </div>
-                    <a href="#" class="d-flex align-items-center justify-content-md-start justify-content-center gap-3">
-                        <i class="fa fa-arrow-right"></i>
-                        <span class="fw-normal text-decoration-underline">Showing Our Latest Reviews *</span>
-                    </a>
                 </div>
             </div>
 </section>
