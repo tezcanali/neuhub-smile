c77f4a331b825d2cf53a193aa911dd57aaf3bc17
Ali Tezcan <alitezcan147@icloud.com>
2024-09-07 11:52:55 +0300

    heading tag update

--- resources/views/components/filament-fabricator/page-blocks/global/breadcrumbs.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/global/breadcrumbs.blade.php
@@ -5,7 +5,7 @@
             <ol class="breadcrumb text-primary mt-2">
                 <li class="breadcrumb-item text-primary"><a href="/">Home</a></li>
                 @if($page->title)
-                    <li class="breadcrumb-item active" aria-current="page" style="color:#475174 !important;"><h1 style="font-size:14px; font-weight:400">{{ $page->title }}</h1></li>
+                    <li class="breadcrumb-item active" aria-current="page" style="color:#475174 !important;">{{ $page->title }}</li>
                 @endif
             </ol>
         </nav>
