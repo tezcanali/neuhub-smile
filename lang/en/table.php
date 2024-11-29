79a883e5e202e294b1d1030618055181b772115b
Ali Tezcan <alitezcan147@icloud.com>
2024-11-29 13:46:38 +0300

    lang updates

---
+++ lang/en/table.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'labels' => [
+        'image' => 'Image',
+        'title' => 'Title',
+        'name' => 'Name',
+        'status' => 'Status',
+        'jobTitle' => 'Job Title',
+    ],
+];
