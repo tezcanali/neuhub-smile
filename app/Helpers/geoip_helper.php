895d276a22045792f61db92fe45b3b287aaf09b8
d3pi3dr4 <denizsinann07@gmail.com>
2025-01-28 11:16:21 +0300

    test for ip updated

--- app/Helpers/geoip_helper.php
+++ app/Helpers/geoip_helper.php
@@ -3,22 +3,19 @@
 use Stevebauman\Location\Facades\Location;
 
 if (!function_exists('get_country_code')) {
-    /**
-     * Kullanıcının IP adresine göre ülke kodunu döndürür.
-     * Türkiye'deyse 1, diğer ülkelerdeyse 2 döner.
-     *
-     * @return int
-     */
     function get_country_code()
     {
-        $ip = request()->ip();
+        try {
+            $ip = request()->ip();
+            $location = Location::get($ip);
 
-        $location = Location::get($ip);
+            if ($location && $location->countryCode === 'TR') {
+                return true;
+            }
 
-        if ($location && $location->countryCode === 'TR') {
-            return true;
+            return false;
+        } catch (\Exception $e) {
+            return false;
         }
-
-        return false;
     }
 }
